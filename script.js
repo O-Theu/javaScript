775ff980f425adb48fd6eb97dc917ad9272bc662
O-Theu <omateus.conceicao@gmail.com>
2022-10-09 14:24:51 -0300

    tipos de dados

---
+++ script.js
@@ -0,0 +1,46 @@
+//VAR
+function variavel(){
+    var altura = 5;
+    var comprimento = 7;
+
+    area = altura * comprimento;
+    console.log(area);
+    var area;
+}
+
+//LET
+function leti(){
+    let forma ='retângulo'
+    let altura = 5;
+    let comprimento = 7;
+    let area;
+
+    if(forma === 'retângulo'){
+        area = altura * comprimento;
+    } else {
+        area = (altura * comprimento) / 2;
+    }
+
+    console.log(area);
+}
+
+//CONST
+function Constante(){
+
+    const forma ='quadrado'
+    const altura = 5;
+    const comprimento = 7;
+    let area;
+
+    if(forma === 'retângulo'){
+        area = altura * comprimento;
+    } else {
+        area = (altura * comprimento) / 2;
+    }
+
+    console.log(area);    
+}
+
+
+
+
